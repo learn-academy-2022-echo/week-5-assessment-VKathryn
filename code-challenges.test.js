0a3708f1b545cd02c4b7a140092dc3d80db1b10d
VKathryn <vkbastien@gmail.com>
2022-09-15 09:51:57 -0700

    ruby and js challenges completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -12,6 +12,8 @@
 
 // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
 
+// PC: create a function that takes in a string, maps through the string, identifies the values asked for and replaces them with the alternate values provided.
+
 // a) Create a test with expect statements using the variables provided.
 
 const secretCodeWord1 = "Lackadaisical"
@@ -21,9 +23,27 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe("codeWord", () => {
+    it("Takes in a string and replaces the identified words", () => {
+      expect(codeWord(secretCodeWord1)).toEqual(`L4ck4d41s1c4l`);
+      expect(codeWord(secretCodeWord2)).toEqual(`G0bbl3dyg00k`);
+      expect(codeWord(secretCodeWord3)).toEqual(`3cc3ntr1c`);
+    });
+  });
+
+//   ReferenceError: codeWord is not defined
 // b) Create the function that makes the test pass.
 
+    const codeWord = (str) => {
+        let dictionary = {"a": 4, "A": 4, "e": 3, "E":3, "i": 1, "I": 1, "o": 0, "O": 0}
+        const transform = letter => dictionary.hasOwnProperty(letter) ? dictionary[letter] : letter 
+    return str.split('').map(transform).join('')
+    }
+
+
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
+
+// PS: create a function that takes in an array of words, and one letter, map through the array, use includes() method, and use if/else statement to return the string if it returns true, put into new array Use .filter to remove the elements that don't belong. 
 
 // a) Create a test with expects statement using the variable provided.
 
@@ -34,11 +54,52 @@ const letterA = "a"
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
-// b) Create the function that makes the test pass.
+describe("fruitArr", () => {
+    it("Takes in an array and returns an array of the words that include a letter identified", () => {
+      expect(fruitArr(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"]);
+      expect(fruitArr(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"]);
+    });
+  });
+
+// //   ReferenceError: fruitArr is not defined
+// // b) Create the function that makes the test pass.
+
+// const fruitArr, takes in Array and letter, returns new array created by using .filter to sort through fruitArr & return only the words that contain the letter in upper or lowercase.
+
+// const fruitArr = (fruitArray, value) => {
+//     newArr = []
+//     if fruitArray.includes(value) == true
+//      newArr.push()
+
+// }
+// const fruitArr = (fruitArray, value) => {
+//     let dictionary = {"a": 4, "A": 4, "e": 3, "E":3, "i": 1, "I": 1, "o": 0, "O": 0}
+//     const transform = letter => dictionary.hasOwnProperty(letter) ? dictionary[letter] : letter 
+// return str.split('').map(transform).join('')
+// }
+
+const fruitArr = (array, letter) => {
+    let newArr = array.filter(word => word.includes(letter) || word.includes(letter.toUpperCase()));
+    return newArr
+}
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
+// PC: Input will be an array, and output will be a boolean value. Check for number of unique elements, check for number of unique elements in array, if not 2 return false, if 2, check how many of each values is contained in the array, if not 1 for either element, return true
+
+
 // a) Create a test with expect statements using the variable provided.
+
+describe("fullHouse", () => {
+    it("determines if the array contains a full house", () => {
+      expect(fullHouse(hand1)).toEqual(true);
+      expect(fullHouse(hand2)).toEqual(false);
+      expect(fullHouse(hand3)).toEqual(false);
+      expect(fullHouse(hand4)).toEqual(true);
+    });
+  });
+
+//   ReferenceError: fullHouse is not defined
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -51,5 +112,44 @@ const hand4 = [7, 2, 7, 2, 7]
 
 // b) Create the function that makes the test pass.
 
+// // this tells us which unique values are present, a full house must have TWO values
 
-pushing up 
+// PC Function fullHouse takes in an array, use Set.size to determine number of values present, if more than 2 are present return false, create a variable with .sort to put values in order, then use if/else conditionals to further clarify that in order to return true, the first two values must equal each other while the final three values equal each other, OR the first three values equal each other and the final two values equal each other. 
+
+const fullHouse = (array) => {
+    const uniqueCount = new Set(array).size
+    const sortArray = array.sort()
+    if (uniqueCount !== 2) {
+        return false
+    }  else if (sortArray[0] === sortArray[1] && sortArray[2] === sortArray[3] && sortArray[3] === sortArray[4]){
+        return true
+    } else if (sortArray[0] === sortArray[1] && sortArray[1] === sortArray[2] && sortArray[3] === sortArray[4]){
+            return true
+    } else { 
+        return false
+    }
+} 
+
+// const fullHouse = (array) => {
+//     array.map((a, v, i) => {
+//         // let a = 0
+//         // (v === val ? a + 1 : a)
+//         console.log(v === a[i])
+//         // if a[i]
+//         return array
+//     })
+// }
+
+// console.log(fullHouse);
+
+// // this counts how many (.size only works on Set!)
+// const uniqueCount = new Set(hand1).size;
+// if (uniqueCount !== 2) {
+//     return false }  
+// else {
+//     return true
+//     }
+
+    // console.log(uniqueCount)
+
+    
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -3,6 +3,10 @@
 
 # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. Use the test variables provided.
 
+# write code that iterates through an array looking for words containing a value, if the word contains the value, return the word in a new array
+
+# Create a function named drink_finder that takes in two values, create a variable that creates an array that will contain the logic that filters through the input, and returns only values that contain the second value provided. 
+
 beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
 letter_o = 'o'
@@ -11,18 +15,102 @@ letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
 
+# beverages_array.select { |item| item.include? (letter_o)} 
+     
+
+def drink_finder = item.select do |item, letter|
+     item.include? (letter)
+end
+# end
+def drink_finder (item, letter)
+     letter_array = item.filter do |value|
+               value.include? (letter)
+          end
+     letter_array
+end
+
+p drink_finder(beverages_array, letter_t)
+
+
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
 
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Vermont'] }
+us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], northeast: ['Maine', 'New Hampshire', 'Vermont'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Vermont', 'Washington'] 
+
+# p us_states.values
+# [["Washington", "Oregon", "Idaho"], ["California", "Arizona", "Nevada"], ["Maine", "New Hampshire", "Vermont"]]
+
+# p us_states.values.flatten
+# ["Washington", "Oregon", "Idaho", "California", "Arizona", "Nevada", "Maine", "New Hampshire", "Vermont"]
+
+
+# p us_states.values.flatten.sort
+# ["Arizona", "California", "Idaho", "Maine", "Nevada", "New Hampshire", "Oregon", "Vermont", "Washington"]
+
+def state_sort hash
+     hash.values.flatten.sort 
+ end
+ 
+ p state_sort(us_states)
+
+# I know .flatten is needed to eliminate the nested array issue, but I'm not sure where it goes yet. 
+
+# new_array = [us_states.map {|x| x.values_at}]
+# prints nothing in terminal
+
+# new_array = [us_states.map {|x| x.values}]
+# "prints: 43:in `block in <main>': undefined method `values' for [:northwest, ["Washington", "Oregon", "Idaho"]]:Array (NoMethodError)
+# Did you mean?  values_at"
+
+# p us_states.map {|x| x.values_at}
+# prints: '[[], [], []]'
+
+# p us_states.map {|x| x.values}
+# prints: '43:in `block in <main>': undefined method `values''
+
+# states_values = (us_states.map {|states| [states["northwest"], states["southwest"], states["northeast"]]})
+# p states_values
+# prints: ':43:in `[]': no implicit conversion of String into Integer'
+
+# puts "Array flatten implementation."
+# # print us_states.flatten
+# prints: '[:northwest, ["Washington", "Oregon", "Idaho"], :southwest, ["California", "Arizona", "Nevada"], :northeast, ["Maine", "New Hampshire", "Vermont"]]%  '
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+# Class Bike
+#      def initialize(model, wheels, speed)
+#          @model = model
+#          @wheels = wheels
+#          @speed = speed
 
+#          puts "The #@model bike has #@wheels and is going #@speed mph"
+#      end
+# end
+
+# trek = Bike.new("Trek", "2", "0")
+
+class Bike
+     def initialize(model)
+         @model = model
+         @wheels = 2
+         @speed = 0 
+     end
+         
+     def bike_info 
+        puts "The #@model bike has #@wheels wheels and is going #@speed mph."
+     end
+
+end
+
+trek = Bike.new("Trek")
+trek.bike_info
+
+# The Trek bike has 2 wheels and is going 0 mph. < Returns correctly!
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -31,4 +119,39 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
 
-pushing up
+class Bike
+     attr_accessor :model, :wheels, :speed
+      def initialize(model)
+          @model = model
+          @wheels = 2
+          @speed = 0 
+      end
+          
+      def bike_info 
+         puts "The #@model bike has #@wheels wheels and is going #@speed mph."
+      end
+ 
+      def pedal_faster(number)
+         @speed += number
+      end
+      def brake(number)
+         if ((@speed - number) >= 0)
+             @speed -= number  
+         else 
+             @speed = 0
+         end  
+      end
+ end
+ 
+ 
+ 
+ trek = Bike.new("Trek")
+ trek.bike_info
+ trek.pedal_faster(2)
+ trek.bike_info
+ trek.pedal_faster(5)
+ trek.bike_info
+ trek.brake(12)
+ trek.bike_info
+
+ # PC Brake subtracts a number from speed. Speed needs to have a floor of 0. if speed equals less than 0 return 0 
